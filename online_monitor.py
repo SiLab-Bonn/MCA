e4396e992d1de737fd634a6e0b1d34012f53f108
Michael Daas <michael.daas@uni-bonn.de>
2015-08-26 16:47:47 +0200

    Initial commit

---
+++ online_monitor.py
@@ -0,0 +1,191 @@
+import sys
+import zmq
+import time
+import numpy as np
+
+from basil import dut
+
+from PyQt4 import Qt
+from PyQt4.QtCore import pyqtSlot, pyqtSignal
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+from pyqtgraph.dockarea import DockArea, Dock
+import pyqtgraph.ptime as ptime
+from threading import Event
+
+from pybar.analysis.RawDataConverter.data_interpreter import PyDataInterpreter
+from pybar.analysis.RawDataConverter.data_histograming import PyDataHistograming
+
+n_bins = 512
+
+class DataWorker(QtCore.QObject):
+    run_start = QtCore.pyqtSignal()
+    config_data = QtCore.pyqtSignal(dict)
+    interpreted_data = QtCore.pyqtSignal(dict)
+    meta_data = QtCore.pyqtSignal(dict)
+    finished = QtCore.pyqtSignal()
+
+    def __init__(self):
+        QtCore.QObject.__init__(self)
+        self.integrate_readouts = 1
+        self._stop_readout = Event()
+        self.hist_range = (0, 2**14)
+        self.histogram = np.zeros(n_bins)
+        
+    def connect(self, socket_addr):
+        self.socket_addr = socket_addr
+        self.context = zmq.Context()
+        self.socket_pull = self.context.socket(zmq.PULL)
+        self.socket_pull.connect(self.socket_addr)
+        self.poller = zmq.Poller()  # poll needed to be able to return QThread
+        self.poller.register(self.socket_pull, zmq.POLLIN)
+
+    def on_set_integrate_readouts(self, value):
+        self.integrate_readouts = value
+
+    def process_data(self):  # infinite loop via QObject.moveToThread(), does not block event loop
+        while(not self._stop_readout.wait(0.001)):  # use wait(), do not block here
+            try:
+                meta_data = self.socket_pull.recv_json(flags=zmq.NOBLOCK)
+                data = self.socket_pull.recv()
+                # reconstruct numpy array
+                buf = buffer(data)
+                dtype = meta_data.pop('dtype')
+                shape = meta_data.pop('shape')
+                threshold = meta_data.pop('thr')
+                data_array = np.frombuffer(buf, dtype=dtype).reshape(shape)
+
+                self.histogram += np.histogram(np.amax(data_array), bins=self.histogram.shape[0], range=self.hist_range)[0]
+                self.interpreted_data.emit({
+                                            "waveform": data_array,
+                                            "histogram": self.histogram,
+                                            "threshold": threshold
+                                            })
+            except zmq.Again:
+                pass
+
+        self.finished.emit()
+
+    def stop(self):
+        self._stop_readout.set()
+
+
+class OnlineMonitorApplication(QtGui.QMainWindow):
+
+    def __init__(self, socket_addr):
+        super(OnlineMonitorApplication, self).__init__()
+        self.setup_plots()
+        self.add_widgets()
+        self.fps = 0
+        self.eps = 0  # events per second
+        self.total_events = 0
+        self.updateTime = ptime.time()
+        self.total_events = 0
+        self.setup_data_worker_and_start(socket_addr)
+
+    def closeEvent(self, event):
+        super(OnlineMonitorApplication, self).closeEvent(event)
+        # wait for thread
+        self.worker.stop()
+        self.thread.wait(1) # fixes message: QThread: Destroyed while thread is still running
+
+    def setup_data_worker_and_start(self, socket_addr):
+        self.thread = QtCore.QThread()  # no parent
+        self.worker = DataWorker()  # no parent
+        self.worker.interpreted_data.connect(self.on_interpreted_data)
+        self.worker.run_start.connect(self.on_run_start)
+        self.worker.config_data.connect(self.on_config_data)
+        self.spin_box.valueChanged.connect(self.worker.on_set_integrate_readouts)
+        self.worker.moveToThread(self.thread)
+        self.worker.connect(socket_addr)
+        self.thread.started.connect(self.worker.process_data)
+        self.worker.finished.connect(self.thread.quit)
+        self.thread.start()
+
+    def setup_plots(self):
+        pg.setConfigOption('background', 'w')
+        pg.setConfigOption('foreground', 'k')
+
+    def add_widgets(self):
+        # Main window with dock area
+        self.dock_area = DockArea()
+        self.setCentralWidget(self.dock_area)
+
+        # Docks
+        dock_waveform = Dock("Waveform", size=(600, 400))
+        dock_histogram = Dock("Histogram", size=(600, 400))
+        dock_status = Dock("Status", size=(1200, 40))
+        self.dock_area.addDock(dock_waveform, 'left')
+        self.dock_area.addDock(dock_histogram, 'right', dock_waveform)
+        self.dock_area.addDock(dock_status, 'top')
+
+        # Status widget
+        cw = QtGui.QWidget()
+        cw.setStyleSheet("QWidget {background-color:white}")
+        layout = QtGui.QGridLayout()
+        cw.setLayout(layout)
+        self.event_rate_label = QtGui.QLabel("Event Rate\n0 Hz")
+        self.spin_box = Qt.QSpinBox(value=20, maximum=1000)
+        layout.addWidget(self.event_rate_label, 0, 4, 0, 1)
+        layout.addWidget(self.spin_box, 0, 6, 0, 1)
+        dock_status.addWidget(cw)
+
+        # Different plot docks
+        waveform_widget = pg.PlotWidget(background="w")
+        self.waveform_plot = waveform_widget.plot(range(0, 200), np.zeros(shape=(200)))
+        dock_waveform.addWidget(waveform_widget)
+
+        histogram_widget = pg.PlotWidget(background="w")
+        self.histogram_plot = histogram_widget.plot(range(0, n_bins + 1), np.zeros(shape=(n_bins)), stepMode=True)
+        histogram_widget.showGrid(y=True)
+        dock_histogram.addWidget(histogram_widget)
+        
+        self.thr_line = pg.InfiniteLine(pos=1000, angle=0, pen={'color':0.0, 'style':QtCore.Qt.DashLine})
+        waveform_widget.addItem(self.thr_line)
+        
+        self.thr_line_hist = pg.InfiniteLine(pos=100, angle=90, pen={'color':0.0, 'style':QtCore.Qt.DashLine})
+        histogram_widget.addItem(self.thr_line_hist)
+
+    @pyqtSlot()
+    def on_run_start(self):
+        pass
+
+    @pyqtSlot(dict)
+    def on_config_data(self, config_data):
+        pass
+
+    def setup_config_text(self, conf):
+        pass
+
+    @pyqtSlot(dict)
+    def on_interpreted_data(self, interpreted_data):
+        self.update_plots(**interpreted_data)
+
+    def update_plots(self, waveform, histogram, threshold):
+        self.total_events += 1
+        if self.spin_box.value() > 0 and self.total_events % self.spin_box.value() == 0:  # only refresh plot every spin_box.value() readout 
+            self.waveform_plot.setData(x=range(0, 200), y=waveform, fillLevel=0, brush=(0, 0, 255, 150))
+            self.histogram_plot.setData(x=range(0, n_bins + 1), y=histogram, fillLevel=0, brush=(0, 0, 255, 150))
+            self.thr_line.setValue(threshold)
+            self.thr_line_hist.setValue(threshold*n_bins/(2**14))
+            self.update_monitor()
+
+    def update_monitor(self):
+        now = ptime.time()
+        recent_eps = 1. / (now - self.updateTime) * self.spin_box.value()
+        self.updateTime = now
+        self.eps = self.eps * 0.9 + recent_eps * 0.1
+        if self.spin_box.value() == 0:  # show number of events
+            self.event_rate_label.setText("Total Events\n%d" % int(self.total_events))
+        else:
+            self.event_rate_label.setText("Event Rate\n%d Hz" % int(self.eps))
+
+
+if __name__ == '__main__':
+    app = Qt.QApplication(sys.argv)
+#     app.aboutToQuit.connect(myExitHandler)
+    win = OnlineMonitorApplication(socket_addr='tcp://127.0.0.1:5678')
+    win.resize(800, 840)
+    win.setWindowTitle('Online Monitor')
+    win.show()
+    sys.exit(app.exec_())
---
+++ qmca.py
@@ -0,0 +1,119 @@
+from basil.dut import Dut
+import numpy as np
+import time
+import zmq
+import progressbar
+import csv
+import tables as tb
+
+#np.set_printoptions(formatter={'int':hex})
+
+def send_data(data, name='qMCA'):
+    '''Sends the data of every read out via ZeroMQ to a specified socket
+    '''
+    try:
+        data_meta_data = dict(
+            name=name,
+            dtype=str(data.dtype),
+            shape=data.shape,
+            thr = threshold
+        )
+        socket.send_json(data_meta_data, flags=zmq.SNDMORE | zmq.NOBLOCK)
+        socket.send(data, flags=zmq.NOBLOCK)  # PyZMQ supports sending numpy arrays without copying any data
+    except zmq.Again:
+        pass
+
+def record_data():
+    # Read data from SRAM
+    single_data = dut['DATA_FIFO'].get_data()
+    #print single_data
+    selection = np.where(single_data & 0x10000000 == 0x10000000)[0]
+    event_data = np.bitwise_and(single_data, 0x00003fff).astype(np.uint32)
+    event_data = np.split(event_data, selection)
+    return event_data   
+
+def set_threshold_and_channel(threshold, channel):
+    dut['TH']['TH'] = int(threshold)
+    dut['TH']['SEL_ADC_CH'] = channel
+    dut['TH'].write()
+    return
+
+def set_adc(scount, delay):
+    dut['fadc0_rx'].set_data_count(scount)
+    dut['fadc0_rx'].set_single_data(True)
+    dut['fadc0_rx'].set_delay(delay)
+    dut['fadc0_rx'].set_en_trigger(True)
+    return
+
+    
+if __name__ == '__main__':
+    # Initialize BASIL DUT
+    dut = Dut('qmca.yaml')
+    dut.init()
+    
+    print "Found Sourcemeter: ", dut['Sourcemeter'].get_name()
+    # Set up internal power supply    
+    dut['PWR0'].set_current_limit(100, unit='mA')
+    dut['PWR0'].set_voltage(1.8, unit='V')
+    dut['PWR0'].set_enable(True)
+    
+    dut['PWR1'].set_current_limit(100, unit='mA')
+    dut['PWR1'].set_voltage(1.8, unit='V')
+    dut['PWR1'].set_enable(True)
+    
+    dut['PWR2'].set_current_limit(100, unit='mA')
+    dut['PWR2'].set_voltage(1.8, unit='V')
+    dut['PWR2'].set_enable(True)
+    
+    dut['PWR3'].set_current_limit(100, unit='mA')
+    dut['PWR3'].set_voltage(1.8, unit='V')
+    dut['PWR3'].set_enable(True)
+    
+    # Set dynamic range of differential ADC
+    dut['VSRC3'].set_voltage(1.9, unit='V')
+    
+    # Settings
+    scount = 200                        # Number of samples per event
+    delay = 50                          # Number of samples before trigger
+    threshold = 2000                    # Default discriminator threshold to define event
+    duration = 60*20                    # Duration of the measurement in seconds
+    filename = 'event_data.h5'          # h5 file name for data
+    socket_addr='tcp://127.0.0.1:5678'  # address to send the data to
+    
+    # Clear FIFO and data-array
+    dut['DATA_FIFO'].get_data()
+    
+    # Setup ZeroMQ socket    
+    socket = zmq.Context().socket(zmq.PUSH)  # push data non blocking
+    socket.bind(socket_addr)
+    
+    # Set ADC settings
+    set_threshold_and_channel(threshold, 3) # Choose channel here
+    set_adc(scount, delay)
+    
+    start_time = time.time()
+    event_count = 0
+    # Pull the FIFO data, split at events, and send/store every event data
+    print("Now recording data for %d seconds..." % (duration))
+    with tb.open_file(filename, 'w') as output_file:
+        output_array = output_file.createEArray(output_file.root, name='event_data', atom=tb.UIntAtom(), shape=(0, 200), title='The raw data of the ADC', filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False))  # expectedrows = ???
+        pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=duration, poll=10, term_width=80).start()
+        while ((time.time() - start_time) < duration):
+            pbar.update(time.time() - start_time)
+            events_data = record_data()
+            for event_data in events_data:
+                if event_data.shape[0] == 200:
+                    send_data(event_data)
+                    output_array.append(event_data.reshape(1, 200))  # whatever...
+                    event_count += 1
+                    if (event_count % 1000 == 0):
+                        output_array.flush()
+        output_array.flush()
+        pbar.finish()
+    
+    print("Done! Recorded %1i events." % event_count)
+    print "Shutting down Sourcemeter..."    
+    #dut['Sourcemeter'].off()
+    print "Done."
+    
+    
