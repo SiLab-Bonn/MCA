c76c0567afd4c6156e8c02ca160fd5ca456026e9
Michael Daas <michael.daas@uni-bonn.de>
2015-08-31 09:46:42 +0200

    ENH: Add biasing and MIO power management

--- source_scan.py
+++ source_scan.py
@@ -4,15 +4,36 @@ import logging
 import time
 import progressbar
 
-duration = 5000
+duration = 10
+
 
 dut = Dut('devices.yaml')
 dut.init()
+logging.info('Found TTI: ' + dut['TTi'].get_name())
 logging.info('Found Sourcemeter: ' + dut['Sourcemeter'].get_name())
 
-my_qmca = qmca.qmca()
+
+bias_voltage = 30
+current_limit = 0.000105
+my_qmca = qmca.qmca(channel=1, threshold=2500)
 
 pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=duration, poll=10, term_width=80).start()
+
+#Powercycle TTI
+dut['TTi'].off()
+dut['TTi'].on()
+time.sleep(1)
+
+#Ramp up bias voltage
+dut['Sourcemeter'].off()
+dut['Sourcemeter'].set_voltage(0)
+dut['Sourcemeter'].set_current_limit(current_limit)
+dut['Sourcemeter'].on()
+for v in range(0,bias_voltage):
+    dut['Sourcemeter'].set_voltage(v)
+    time.sleep(0.5)
+
+
 
 my_qmca.start()
 start_time = time.time()
@@ -25,3 +46,10 @@ while (actual_time < (start_time + duration)):
 
 my_qmca.stop()
 pbar.finish()
+
+#Ramp down bias voltage
+for v in range(bias_voltage,0, -1):
+    dut['Sourcemeter'].set_voltage(v)
+    time.sleep(0.5)
+    
+dut['Sourcemeter'].off()
