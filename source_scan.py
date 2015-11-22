ba8f4364767e2e5d55548fe746f5be413a018063
Michael Daas <michael.daas@uni-bonn.de>
2015-11-22 14:00:09 +0100

    Customized for November testbeam

--- source_scan.py
+++ source_scan.py
@@ -1,45 +1,31 @@
 import qmca
 from basil.dut import Dut
-import logging
 import time
 import progressbar
 
-duration = 10
-
-
-dut = Dut('devices.yaml')
-dut.init()
-logging.info('Found TTI: ' + dut['TTi'].get_name())
-logging.info('Found Sourcemeter: ' + dut['Sourcemeter'].get_name())
-
-
+duration = 60*60
+max_events = 1000000
 bias_voltage = 30
 current_limit = 0.000105
-my_qmca = qmca.qmca(channel=1, threshold=2500)
+my_qmca = qmca.qmca(channel=1,
+                    threshold=3000,
+                    sample_count=200,
+                    sample_delay=50,
+                    write_after_n_events=1000000,
+                    adc_differential_voltage=1.5)
 
 pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=duration, poll=10, term_width=80).start()
 
-#Powercycle TTI
-dut['TTi'].off()
-dut['TTi'].on()
-time.sleep(1)
-
-#Ramp up bias voltage
-dut['Sourcemeter'].off()
-dut['Sourcemeter'].set_voltage(0)
-dut['Sourcemeter'].set_current_limit(current_limit)
-dut['Sourcemeter'].on()
-for v in range(0,bias_voltage):
-    dut['Sourcemeter'].set_voltage(v)
-    time.sleep(0.5)
-
-
-
-my_qmca.start()
 start_time = time.time()
 actual_time = start_time
 
-while (actual_time < (start_time + duration)):
+my_qmca.start()
+
+# print 'event_count is %d' % my_qmca.event_count
+
+while ((actual_time < (start_time + duration)) and (my_qmca.event_count <= max_events)):
+# while True:
+#     print 'event_count is %d' % my_qmca.event_count
     pbar.update(actual_time - start_time)
     actual_time = time.time()
     time.sleep(1)
@@ -48,8 +34,8 @@ my_qmca.stop()
 pbar.finish()
 
 #Ramp down bias voltage
-for v in range(bias_voltage,0, -1):
-    dut['Sourcemeter'].set_voltage(v)
-    time.sleep(0.5)
-    
-dut['Sourcemeter'].off()
+# for v in range(bias_voltage,0, -1):
+#     dut['Sourcemeter'].set_voltage(v)
+#     time.sleep(0.5)
+#     
+# dut['Sourcemeter'].off()
