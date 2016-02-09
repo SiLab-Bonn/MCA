3314f85431a018b5d8185fff0f5fa49fcdd757ff
michael <michael@silab23>
2016-02-09 14:47:10 +0100

    MAINT: Cleanup

--- source_scan.py
+++ source_scan.py
@@ -1,17 +1,15 @@
 import qmca
-from basil.dut import Dut
 import time
 import progressbar
 
-duration = 60*60
+duration = 60*30
 max_events = 1000000
-bias_voltage = 30
-current_limit = 0.000105
-my_qmca = qmca.qmca(channel=1,
-                    threshold=3000,
+
+my_qmca = qmca.qmca(channel=2,
+                    threshold=6000,
                     sample_count=200,
                     sample_delay=50,
-                    write_after_n_events=1000000,
+                    write_after_n_events=10000,
                     adc_differential_voltage=1.5)
 
 pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=duration, poll=10, term_width=80).start()
@@ -20,22 +18,10 @@ start_time = time.time()
 actual_time = start_time
 
 my_qmca.start()
-
-# print 'event_count is %d' % my_qmca.event_count
-
 while ((actual_time < (start_time + duration)) and (my_qmca.event_count <= max_events)):
-# while True:
-#     print 'event_count is %d' % my_qmca.event_count
     pbar.update(actual_time - start_time)
     actual_time = time.time()
     time.sleep(1)
 
 my_qmca.stop()
 pbar.finish()
-
-#Ramp down bias voltage
-# for v in range(bias_voltage,0, -1):
-#     dut['Sourcemeter'].set_voltage(v)
-#     time.sleep(0.5)
-#     
-# dut['Sourcemeter'].off()
